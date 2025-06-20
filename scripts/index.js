76f533eb5c60a2a7fee875fd88709452c83d77c6
DAVID CHANN <DNARETC@GMAIL.COM>
2025-06-19 22:49:45 -0700

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
-const editProfileBtn = ocument.querySelector(".profile__edit-btn");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editPRofileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const editProofileForm = editProfileModel.querySelector(".modal__forum")
-const editlProfileNameInput = editProfileModel.querySelector("#profile-name-input")
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__forum")
+const editlProfileNameInput = editProfileModal.querySelector("#profile-name-input")
 const editlProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input")
 
 const newPostBtn = document.querySelector(".profile__newpost-btn");
 const newPostmodal = document.querySelector("#new-post-modal");
-const newPstCLosdBtn = document.querySelector("modal__close-btn");
+const newPostCLosdBtn = document.querySelector("modal__close-btn");
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEL = document.querySelector(".profile__description");
