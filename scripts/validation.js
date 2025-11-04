3c0e73ef53a4da1c7400611c759e56782a74cd15
DAVID CHANN <DNARETC@GMAIL.COM>
2025-11-03 22:44:27 -0800

    New update for my project

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,27 +1,27 @@
-export function enableValidation(config) {
+function enableValidation(config) {
   const forms = Array.from(document.querySelectorAll(config.formSelector));
-
+ 
   forms.forEach(form => {
     const inputs = Array.from(form.querySelectorAll(config.inputSelector));
     const submitButton = form.querySelector(config.submitButtonSelector);
-
+ 
     function showInputError(input) {
       const error = form.querySelector(`#${input.id}-error`);
       input.classList.add(config.inputErrorClass);
       error.textContent = input.validationMessage;
     }
-
+ 
     function hideInputError(input) {
       const error = form.querySelector(`#${input.id}-error`);
       input.classList.remove(config.inputErrorClass);
       error.textContent = "";
     }
-
+ 
     function checkInputValidity(input) {
       if (!input.validity.valid) showInputError(input);
       else hideInputError(input);
     }
-
+ 
     function toggleButtonState() {
       if (form.checkValidity()) {
         submitButton.disabled = false;
@@ -31,14 +31,13 @@ export function enableValidation(config) {
         submitButton.classList.add(config.inactiveButtonClass);
       }
     }
-
+ 
     inputs.forEach(input => {
       input.addEventListener("input", () => {
         checkInputValidity(input);
         toggleButtonState();
       });
     });
-
-    toggleButtonState(); // initialize button state
-  });
+ 
+    toggleButtonState(); 
 }
